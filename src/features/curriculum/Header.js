72257581fbb8926730ba83bb68cc22aa1011a6ee
Willian Watanabe <watinha@gmail.com>
2020-04-27 19:09:51 -0300

    completing header information

--- src/features/curriculum/Header.js
+++ src/features/curriculum/Header.js
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import ReactHtmlParser from 'react-html-parser';
 
 import Loader from './Loader';
-import { loadCurriculum, map_title, map_address } from './curriculumSlice';
+import { loadCurriculum, map_contact, map_title, map_address } from './curriculumSlice';
 
 export default function Header () {
 	const dispatch = useDispatch(),
           title = useSelector(map_title),
-          address = useSelector(map_address);
+          address = useSelector(map_address),
+          contact = useSelector(map_contact);
 
     useEffect(() => {
         dispatch(loadCurriculum());
@@ -17,7 +19,16 @@ export default function Header () {
         <header className="header">
             <Loader />
 			<h1>{title}</h1>
-            <p className="address">{address}</p>
+            <p className="address">{ReactHtmlParser(address)}</p>
+            <ul className="contact">
+            {contact && contact.map((con, id) => {
+                return (
+                    <li key={id}>
+                        <a href={con.link} className={"icon " + con.icon_label}>{con.label}</a>
+                    </li>
+                );
+            })}
+            </ul>
         </header>
     );
 }
--- src/features/curriculum/Header.test.js
+++ src/features/curriculum/Header.test.js
@@ -33,7 +33,13 @@ it('renders a loader as ajax is started', async () => {
 
 it('renders a header and do not show loader after ajax succeeds', async () => {
     const response = Promise.resolve({
-        data: { title: 'some title', address: 'somewhere' } });
+        data: {
+            title: 'some title', address: 'somewhere',
+            contacts: [
+                { link: 'http://bar.cool', label: 'bar', icon_label: 'cool' },
+                { link: 'http://another.cool', label: 'superbar', icon_label: 'nothing' }
+            ]
+        } });
     jest.spyOn(axios, 'get').mockImplementation((url) => {
         expect(url).toBe('./curriculum.json');
         return response;
@@ -48,17 +54,23 @@ it('renders a header and do not show loader after ajax succeeds', async () => {
 
     let loading = container.querySelectorAll('.loader.show'),
 		h1 = container.querySelector('h1'),
-        address = container.querySelector('.address');
+        address = container.querySelector('.address'),
+        contact = container.querySelector('.contact');
     expect(loading.length).toBe(0);
 	expect(h1.innerHTML).toBe('some title');
 	expect(address.innerHTML).toBe('somewhere');
+    expect(contact.querySelectorAll('li').length).toBe(2);
 
     axios.get.mockRestore();
 });
 
 it('renders a header with different value', async () => {
-    const response = Promise.resolve(
-        { data: { title: 'another', address: 'under the rainbow' } });
+    const response = Promise.resolve({ data: {
+        title: 'another', address: 'under the rainbow',
+        contacts: [
+            { link: 'http://foo.bar', label: 'foo', icon_label: 'another' }
+        ]
+    } });
     jest.spyOn(axios, 'get').mockImplementation((url) => {
         expect(url).toBe('./curriculum.json');
         return response;
@@ -73,10 +85,12 @@ it('renders a header with different value', async () => {
 
     let loading = container.querySelectorAll('.loader.show'),
 		h1 = container.querySelector('h1'),
-        address = container.querySelector('.address');
+        address = container.querySelector('.address'),
+        contact = container.querySelector('.contact');
     expect(loading.length).toBe(0);
 	expect(h1.innerHTML).toBe('another');
 	expect(address.innerHTML).toBe('under the rainbow');
+	expect(contact.querySelectorAll('li').length).toBe(1);
 
     axios.get.mockRestore();
 });
