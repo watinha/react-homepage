ebdab0e2e17ebf9d98d9ee09aaa5c1e0adb60f9a
Willian Watanabe <watinha@gmail.com>
2020-04-27 12:46:39 -0300

    starting project react-homepage

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+let container = null;
+beforeEach(() => {
+    container = document.createElement('div');
+});
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+it('renders a header with main information', () => {
+    render(
+        <Provider store={store}>
+            <App />
+        </Provider>, container);
+    expect(container.querySelector('h1')).toBe(null);
 });
