c84419e858dc53b8fc802f9a6e526856d34b6e65
Willian Watanabe <watinha@gmail.com>
2020-04-29 15:15:01 -0300

    refactor: improving the organization of the code

--- src/features/curriculum/TabPanel.js
+++ src/features/curriculum/TabPanel.js
@@ -6,32 +6,11 @@ import Panel from './Panel';
 
 export default function TabPanel () {
     const sections = useSelector(map_sections),
-          [diagonal, setDiagonal] = useState(false),
+          [view, setView] = useState(standard_view),
           [selected, setSelected] = useState(-1);
 
-    const generate_style = (id, length) => {
-        const bottom = 50 * id - 10;
-        if (diagonal && id === selected)
-            return { marginTop: 0, bottom: `${bottom}px`};
-        if (diagonal) return { marginTop: 0 };
-
-        const i = id + 1,
-              left = Math.floor((5 + i*0.9*3) * 100) / 100,
-              scale = Math.floor((6+4*(i/length))*10)/100;
-        return {
-            transform: `scale(${scale}) ` +
-                       "matrix(1, 0, 0, 1, 0, 0)",
-            left: `${left}%`,
-            marginTop: "-4%"
-        };
-    };
-
-    const generate_class = (id) =>
-        `${diagonal && "diagonal"}` +
-        `${id === selected ? " open" : ""}`;
-
     const activate = (id) => {
-        setDiagonal(true);
+        setView(diagonal_view);
         setSelected(id);
     };
 
@@ -39,8 +18,9 @@ export default function TabPanel () {
         <div className="wrapper">
         {sections && sections.map((section, id) =>
         <div key={id}
-             className={generate_class(id)}
-             style={generate_style(id, sections.length)}
+             className={view.generate_class(id, selected)}
+             style={view.generate_style(
+                 id, sections.length, selected)}
              onClick={() => activate(id)}>
             <section className={section.title}>
                 {section.title === 'introduction' &&
@@ -55,3 +35,35 @@ export default function TabPanel () {
         </div>
     );
 }
+
+const standard_view = (() => {
+    return {
+        generate_class: (id) => "",
+        generate_style: (id, length) => {
+            const i = id + 1,
+                  left = Math.floor(
+                      (5 + i*0.9*3) * 100) / 100,
+                  scale = Math.floor(
+                      (6+4*(i/length))*10)/100;
+            return {
+                transform: `scale(${scale}) ` +
+                           "matrix(1, 0, 0, 1, 0, 0)",
+                left: `${left}%`,
+                marginTop: "-4%"
+            };
+        }
+    };
+})();
+
+const diagonal_view = (() => {
+    return {
+        generate_class: (id, selected) =>
+            `diagonal${id === selected ? " open" : ""}`,
+        generate_style: (id, length, selected) => {
+            const bottom = 50 * id - 10;
+            if (id === selected)
+                return {marginTop: 0, bottom: `${bottom}px`};
+            return { marginTop: 0 };
+        }
+    };
+})();
